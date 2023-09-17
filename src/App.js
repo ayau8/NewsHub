c2c54029cd85311ddf655447c9f9d0a1b121c07c
ayau8 <ayaualvin@gmail.com>
2023-09-18 01:12:52 +0900

    basic setup and use of NewsAPI with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Menu from './components/Menu'
+import NewsGrid from "./components/NewsGrid"
 
 function App() {
+  const [items, setItems] = useState([])
+  const [active, setActive] = useState(1)
+  const [category, setCategory] = useState("general")
+
+  useEffect(() => {
+    fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=6e09b99946874e6fbbc5b4b9aea77c73`)
+    .then(res => res.json())
+    .then(data => setItems(data.articles))
+  }, [category])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">See The Latest News</h1>
+      {/* <Menu active={active} setActive={setActive} setCategory={setCategory}/>
+      <NewsGrid items={items}/> */}
     </div>
   );
 }
